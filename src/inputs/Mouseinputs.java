de772a82bd879fff6605e0ea86794d55af45d02d
sanjidakhushi <sanjidakhushi18@gmail.com>
2025-10-15 01:58:43 +0600

    Day 6
    Create a menu screen before entering the game, and add a background to the menu.

--- src/inputs/Mouseinputs.java
+++ src/inputs/Mouseinputs.java
@@ -3,6 +3,8 @@ package inputs;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+
+import gamestates.Gamestate;
 import inputs.Mouseinputs;
 import main.GamePanel;
 
@@ -22,27 +24,63 @@ public class Mouseinputs implements MouseListener ,MouseMotionListener{
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
-		//gamePanel.setRectPos(e.getX(), e.getY());
-		
-	    // System.out.println("mouse moved!");
+	       switch(Gamestate.state) {
+			case MENU:
+				gamePanel.getGame().getMenu().mouseMoved(e);
+				break;
+			case PLAYING:
+				gamePanel.getGame().getPlaying().mouseMoved(e);
+				break;
+			default:
+				break; 
+		 }
+		     
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		 if(e.getButton() == MouseEvent.BUTTON1)
-		    gamePanel.getGame().getPlayer().setAttacking(true);
-		
-	}
+	       switch(Gamestate.state) {
+		case MENU:
+			gamePanel.getGame().getMenu().mouseClicked(e);
+			break;
+		case PLAYING:
+			gamePanel.getGame().getPlaying().mouseClicked(e);
+			break;
+		default:
+			break; 
+	 }
+	       
+}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
+	    switch(Gamestate.state) {
+				case MENU:
+					gamePanel.getGame().getMenu().mousePressed(e);
+					break;
+				case PLAYING:
+					gamePanel.getGame().getPlaying().mousePressed(e);
+					break;
+				default:
+					break; 
+			 }
+			     
 		
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
+	    switch(Gamestate.state) {
+				case MENU:
+					gamePanel.getGame().getMenu().mouseReleased(e);
+					break;
+				case PLAYING:
+					gamePanel.getGame().getPlaying().mouseReleased(e);
+					break;
+				default:
+					break; 
+			 }
+			     
 		
 	}
 
--- src/main/Game.java
+++ src/main/Game.java
@@ -1,9 +1,11 @@
 package main;
 
 import java.awt.Graphics;
+import java.awt.event.MouseListener;
 
-import Levels.LevelManager;
-import entities.Player;
+import gamestates.Gamestate;
+import gamestates.Menu;
+import gamestates.Playing;
 
 public class Game implements Runnable {
 	
@@ -12,11 +14,12 @@ private GamePanel gamePanel;
 private Thread gameThread;
 private final int FPS_SET = 120;
 private  final int UPS_SET = 200;
-private Player player;
-private LevelManager levelManager;
+
+private Playing playing;
+private Menu menu;
 
 public final static int TILES_DEFAULT_SIZE = 32;
-public final static float SCALE = 1f;
+public final static float SCALE = 1.25f;
 public final static int TILES_IN_WIDTH = 26;
 public final static int TILES_IN_HEIGHT = 14;
 public final static int TILES_SIZE = (int) (TILES_DEFAULT_SIZE * SCALE);
@@ -34,9 +37,10 @@ public final static int GAME_HEIGHT = TILES_SIZE * TILES_IN_HEIGHT;
 	}
 	
 	private void initClasses() {
-    levelManager = new LevelManager(this);
-    player = new Player(200 , 200, (int)(64* SCALE), (int)(40* SCALE));
-    player.loadLvlData(levelManager.getCurrentLevel().getLevelData());
+		
+		menu = new Menu(this);
+		playing = new Playing(this);
+    
 	}
 
 	private void startGameLoop() {
@@ -45,16 +49,37 @@ public final static int GAME_HEIGHT = TILES_SIZE * TILES_IN_HEIGHT;
 	}
 	
     public void update() {
-    	levelManager.update();
-    	player.update();
+    	
+    	switch (Gamestate.state) {
+		case MENU:
+			menu.update();
+			break;
+		case PLAYING:
+			playing.update();
+			break;
+		case OPTIONS:
+		case QUIT:
+		default:
+			System.exit(0);
+			break;
+    	
+    	}
 	
 }
 
 public void render(Graphics g) {
-
-	levelManager.draw(g);
-	player.render(g);
 	
+	switch (Gamestate.state) {
+	case MENU:
+		menu.draw(g);
+		break;
+	case PLAYING:
+		playing.draw(g);
+		break;
+	default:
+		break;
+	
+	}
 }
 
 	@Override
@@ -98,17 +123,22 @@ public void render(Graphics g) {
 				System.out.println("FPS: " + frames + "| UPS: " +updates);
 				frames = 0;
 				updates = 0;
-			
 		}
 	}
   
    }
 	
 	public void windowFocusLost() {
-		player.resetDirBooleans();
+		if(Gamestate.state == Gamestate.PLAYING)
+			playing.getPlayer().resetDirBooleans();
+	
 	}
 	
-	public Player getPlayer() {
-		return player;
+	public Menu getMenu() {
+		return menu;
 	}
+	public Playing getPlaying() {
+		return playing;
+	}
+
 }
